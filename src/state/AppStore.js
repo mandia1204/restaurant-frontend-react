aa89819901a757bbdbf4a21d96b90dd9fb8167e2
Marvin Andia <mandia1204@gmail.com>
2018-06-23 09:07:44 -0500

    adding redux_devtools support

--- src/state/AppStore.js
+++ src/state/AppStore.js
@@ -1,8 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
 import { AppReducer } from './reducers/AppReducer';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk, promiseMiddleware())
+    // other store enhancers if any
+);
+
 export const createAppStore = () => {
-    return createStore(AppReducer, applyMiddleware(thunk, promiseMiddleware()));
+    return createStore(AppReducer, enhancer);
 };
