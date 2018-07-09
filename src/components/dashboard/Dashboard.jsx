d8c9b826e45ac9eb36dc9902acf85bd47bf05033
Marvin Andia <mandia1204@gmail.com>
2018-07-09 10:07:15 -0500

    responsive dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -7,58 +7,59 @@ import PropTypes from 'prop-types';
 
 const Dashboard = ({chartModel, anulaciones, cardModel}) => {
     const height = 140;
-    const itemWitdh = 4;
+    const chartWidth = { lg: 4, xl: 4, md: 6, xs:12, sm:12 };
+    const cardWidth = { lg: 3, xl: 3, md: 6, xs:12, sm:12 };
     
     return (
         <Grid container spacing={8}>
-          <Grid item xs={itemWitdh}>
+          <Grid item {...chartWidth}>
               <Bar
               data={chartModel.ventasAnuales.data}
               height={height}
               options={chartModel.ventasAnuales.options} 
               />
           </Grid>
-          <Grid item xs={itemWitdh}>
+          <Grid item {...chartWidth}>
               <Bar
               data={chartModel.mozoMes.data}
               height={height}
               options={chartModel.mozoMes.options}
              />
           </Grid>
-          <Grid item xs={itemWitdh}>
+          <Grid item {...chartWidth}>
               <Bar
               data={chartModel.platoMes.data}
               height={height}
               options={chartModel.platoMes.options} 
               />
           </Grid>
-          <Grid item xs={itemWitdh}>
+          <Grid item {...chartWidth}>
               <Anulaciones anulaciones={anulaciones}/>
           </Grid>
-          <Grid item xs={itemWitdh}>
+          <Grid item {...chartWidth}>
               <Radar
               data={chartModel.anulacionesMes.data}
               height={height}
               options={chartModel.anulacionesMes.options}
               />
           </Grid>
-          <Grid item xs={itemWitdh}>
+          <Grid item {...chartWidth}>
               <Pie
               data={chartModel.productosMes.data}
               height={height}
               options={chartModel.productosMes.options}
               />
           </Grid>
-          <Grid item xs={3}>
+          <Grid item {...cardWidth}>
             <DashboardCard card={cardModel.produccionDia} />
           </Grid>
-          <Grid item xs={3}>
+          <Grid item {...cardWidth}>
             <DashboardCard card={cardModel.paxDia} />
           </Grid>
-          <Grid item xs={3}>
+          <Grid item {...cardWidth}>
             <DashboardCard card={cardModel.ticketPromedioDia} />
           </Grid>
-          <Grid item xs={3}>
+          <Grid item {...cardWidth}>
             <DashboardCard card={cardModel.ventaDia} />
           </Grid>
         </Grid>
