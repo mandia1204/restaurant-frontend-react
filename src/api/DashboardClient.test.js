c24b5c64777c9e36a303899b9300185717e96fa6
Marvin Andia <marvin.andia@avantica.net>
2018-07-19 17:28:09 -0500

    removing proxyquire doesnt support browser

--- src/api/DashboardClient.test.js
+++ src/api/DashboardClient.test.js
@@ -1,26 +1,28 @@
 import tape from 'tape';
 import _test from 'tape-promise';
 import sinon from 'sinon';
-import proxyquire from 'proxyquire';
+import * as wrapperFunctions from '../wrappers/HttpWrapper';
 
 const test = _test(tape);
 
 test('DashboardClient.getDashboard(), passing filters, calls the api and returns promise', (assert) => {
   const filters = { year: 2018, month: 2, ops: 'ABC' };
-  const spyGet = sinon.spy(url => new Promise((resolve) => { resolve({ data: url }); }));
 
-  const HttpWrapperStub = () => ({
+  const spyGet = sinon.spy(url => new Promise((resolve) => {
+    resolve({ data: url });
+  }));
+
+  const httpWrapperStub = sinon.stub(wrapperFunctions, 'HttpWrapper').callsFake(() => ({
     get: spyGet,
-  });
+  }));
 
-  const client = proxyquire('./DashboardClient', {
-    '../wrappers/HttpWrapper': { HttpWrapper: HttpWrapperStub },
-  }).DashboardClient;
+  const dashboardClient = require('./DashboardClient').DashboardClient;// eslint-disable-line global-require
 
   const expectedUrl = '/dashboard?anio=2018&mes=2&ops=ABC';
-  return client().getDashboard(filters).then((response) => {
+  return dashboardClient().getDashboard(filters).then((response) => {
     assert.ok(spyGet.calledOnceWith(expectedUrl), 'http called once with correct url');
     assert.equal(response.data, expectedUrl, 'Data should be returned');
+    httpWrapperStub.restore();
     assert.end();
   });
 });
