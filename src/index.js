020d792dcf2a7f6d52dcf9693645b03cfa6fe2fe
Marvin Andia <mandia1204@gmail.com>
2018-11-13 23:12:08 -0500

    adding docker file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import AppContainer from './components/AppContainer';
 import './index.scss';
 
-// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<AppContainer />, document.getElementById('app'));
 
 if (process.env.NODE_ENV === 'development') {
