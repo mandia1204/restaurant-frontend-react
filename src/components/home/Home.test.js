7142d82fa870f3083f0446990dd5929d9196e914
Marvin Andia <mandia1204@gmail.com>
2019-03-18 12:30:18 -0500

    fixing enzyme tests

--- src/components/home/Home.test.js
+++ src/components/home/Home.test.js
@@ -1,16 +1,16 @@
-// import React from 'react';
-// import test from 'tape';
-// import { shallow } from 'enzyme';
-// import Home from './Home';
-// import '../../../setupTest';
+import React from 'react';
+import test from 'tape';
+import { shallow } from 'enzyme';
+import '../../../setupTest';
+import Home from './Home';
 
-// test('[Home]', (t) => {
-//   t.test('--should render a div', (a) => {
-//     const wrapper = shallow(<Home />);
-//     const el = wrapper.find('div');
+test('[Home]', (t) => {
+  t.test('--should render a div', (a) => {
+    const wrapper = shallow(<Home />);
+    const el = wrapper.find('div');
 
-//     a.equal(el.length, 1);
-//     a.end();
-//   });
-//   t.skip('');
-// });
+    a.equal(el.length, 1);
+    a.end();
+  });
+  t.skip('');
+});
