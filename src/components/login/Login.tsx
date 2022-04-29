8a9e016b14119cd9a0f79b922a16cb58a3bef21a
Marvin Andia <mandia1204@gmail.com>
2022-04-28 23:56:12 -0500

    login converted to functional component

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,56 +1,47 @@
 import React, { Dispatch } from 'react';
-// import { withRouter, RouteComponentProps } from 'react-router'; // eslint-disable-line import/no-extraneous-dependencies
 import { connect } from 'react-redux';
 import { Formik, FormikActions, FormikProps } from 'formik';
+import { useNavigate } from 'react-router-dom';
 import Actions from '../../state/actions/AppActions';
 import LoginForm from './LoginForm';
 import SecurityService from '../../services/SecurityService';
 import ISecurityService from '../../types/ISecurityService';
 import LoginCredentials from '../../types/LoginCredentials';
 
-// interface LoginProps extends RouteComponentProps<any> {
-//   dispatch: Dispatch<any>;
-// }
 interface LoginProps {
   dispatch: Dispatch<any>;
 }
+const securityService: ISecurityService = SecurityService();
 
-class Login extends React.Component<LoginProps, any> {
-  securityService: ISecurityService;
-
-  constructor(props: LoginProps) {
-    super(props);
-    this.securityService = SecurityService();
-  }
-
-  onSubmit = (values: LoginCredentials, { setSubmitting }: FormikActions<LoginCredentials>) => {
-    this.login(values, setSubmitting);
-  };
-
-  loginSuccess = (credentials: LoginCredentials) => {
-    // const { history, dispatch } = this.props;
-    // history.push('/');
-    const { dispatch } = this.props; // TODO: IMPLEMENT history
+function Login(props: LoginProps) {
+  const navigate = useNavigate();
+  const loginSuccess = (credentials: LoginCredentials) => {
+    navigate('/');
+    const { dispatch } = props;
     const { Creators } = Actions;
     dispatch(Creators.updateLoginData({ user: { name: credentials.userName }, authenticated: true }));
   };
 
-  loginFail = () => {
+  const loginFail = () => {
     alert('auth failed'); //eslint-disable-line
   };
 
-  login = (user: LoginCredentials, setSubmitting: (isSubmitting: boolean) => void) => {
-    this.securityService.authenticate(user).then((response: any) => {
+  const login = (user: LoginCredentials, setSubmitting: (isSubmitting: boolean) => void) => {
+    securityService.authenticate(user).then((response: any) => {
       setSubmitting(false);
       if (response.success) {
-        this.loginSuccess(user);
+        loginSuccess(user);
       } else {
-        this.loginFail();
+        loginFail();
       }
     });
   };
 
-  validate = (credentials: LoginCredentials) => {
+  const onSubmit = (values: LoginCredentials, { setSubmitting }: FormikActions<LoginCredentials>) => {
+    login(values, setSubmitting);
+  };
+
+  const validate = (credentials: LoginCredentials) => {
     const errors: any = {};
 
     if (!credentials.userName) {
@@ -61,21 +52,18 @@ class Login extends React.Component<LoginProps, any> {
     }
     return errors;
   };
-
-  render() {
-    return (
-      <Formik
-        initialValues={{
-          userName: 'matt',
-          password: '1234',
-        }}
-        render={(props: FormikProps<LoginCredentials>) => (<LoginForm {...props} />)}
-        onSubmit={this.onSubmit}
-        validate={this.validate}
-        validateOnChange={false}
-      />
-    );
-  }
+  return (
+    <Formik
+      initialValues={{
+        userName: 'matt',
+        password: '1234',
+      }}
+      render={(fprops: FormikProps<LoginCredentials>) => (<LoginForm {...fprops} />)}
+      onSubmit={onSubmit}
+      validate={validate}
+      validateOnChange={false}
+    />
+  );
 }
 
 export default connect()(Login);
