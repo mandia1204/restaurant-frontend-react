2de35b6894dcf22ee88902977d7530da4c87fc28
Marvin Andia <mandia1204@gmail.com>
2022-05-12 19:15:17 -0500

    missing change

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
 import AppContainer from './components/AppContainer';
 import './styles/index.scss';
 import './styles/reset.scss';
 import '@babel/polyfill';
-import theme from './theme';
 
 const root = createRoot(document.getElementById('app'));
 root.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+  <>
     <CssBaseline />
     <AppContainer />
-  </ThemeProvider>,
+  </>,
 );
 
 if (process.env.NODE_ENV === 'development') {
